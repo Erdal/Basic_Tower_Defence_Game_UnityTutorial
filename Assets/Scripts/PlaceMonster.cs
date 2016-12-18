976707ae5e306191ff701ea5bca04ad04c1c5601
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-12-19 01:35:12 +0800

    Can now place monsters on specific places with a single click or tap. Also making sure the user can only place one monster on one spot

---
+++ Assets/Scripts/PlaceMonster.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceMonster : MonoBehaviour
+{
+    public GameObject monsterPrefab; //instantiate a copy of the object stored in monsterPrefab to create a monster,
+    private GameObject monster; //store it in monster so you can manipulate it during the game
+
+    //allow only one monster per location:
+    private bool canPlaceMonster()
+    {
+        return monster == null;
+    }
+
+    //This code places a monster on mouse click or tap
+    //Unity automatically calls OnMouseUp when a player taps a GameObject’s physics collider.
+    void OnMouseUp()
+    {
+        //When called, this method places a new monster if canPlaceMonster() returns true.
+        if (canPlaceMonster())
+        {
+            //You create the monster with Instantiate, a method that creates an instance of a given prefab with the specified position and rotation. In this case, you copy monsterPrefab, give it the current GameObject’s position and no rotation, cast the result to a GameObject and store it in monster.
+            monster = (GameObject)
+              Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+            //Call PlayOneShot to play the sound effect attached to the object’s AudioSource component.
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            // TODO: Deduct gold
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+}
