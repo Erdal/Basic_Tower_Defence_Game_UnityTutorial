027199f703e55e97dcec7318d4f134f5ffd65d87
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-12-19 13:19:08 +0800

    We have created our way points and our first enemy to follow thoes waypoints, our enemy also roates to face the correct direction

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject[] waypoints; //Waypoint array
+    public GameObject testEnemyPrefab; //reference to the Enemy prefab in testEnemyPrefab.
+
+    // Use this for initialization
+    void Start()
+    {
+        Instantiate(testEnemyPrefab).GetComponent<MoveEnemy>().waypoints = waypoints; //create an enemy when the script starts
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+}
